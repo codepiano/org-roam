fca1ced9ddae6dd6665dd23ba920b8f2f169b723
codepiano <codepiano.li@gmail.com>
2021-07-28 16:43:14 +0800

    refactor

--- org-roam-reverie-utils.el
+++ org-roam-reverie-utils.el
@@ -6,4 +6,9 @@
     "format time to iso8601 string with current timezone"
     (car (time-convert (encode-time (iso8601-parse time)) 1000)))
 
+(defun org-roam-reverie-get-created-time-from-path (path)
+  "extract date-time part from file path: 20210728101112, convert to timestamp"
+  (let ((ts (car (split-string (car (last (split-string path "/"))) "-"))))
+    (car (time-convert (encode-time (iso8601-parse (format "%sT%s" (substring ts 0 8) (substring ts 8)))) 1000))))
+
 (provide 'org-roam-reverie-utils)
